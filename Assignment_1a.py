cfa6be504c097da805f45ee9de40dd97db5fb45a
parth753 <parthmakwana787@gmail.com>
2025-07-15 16:39:32 +0530

    Delete Assignment_1a.py

--- Assignment_1a.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter the First Number: "))
-b = int(input("Enter the Second Number: "))
-
-print('Addition:',a+b )
-print('Subtraction:',a-b )
-print('Mutiplication:',a*b )
-print('Division:',a/b )
